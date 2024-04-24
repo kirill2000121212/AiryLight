623dd5f96ed2c6b0bbe7789a73cb21b788c8ab87
kirill2000121212 <alakonnex@gmail.com>
2024-04-24 21:23:41 +0300

    img not normal

--- src/index.js
+++ src/index.js
@@ -6,9 +6,11 @@ import "./shared/scss/animation.scss";
 import "./shared/scss/header.scss";
 import "./shared/scss/sign-up-modal.scss";
 import "./shared/scss/footer.scss";
+import "./shared/scss/section-slider.scss"
 
 import "./shared/js/sign-up.js"
 import "./shared/js/validate-form.js"
+import "./shared/js/slider-how-work.js"
 
 
 

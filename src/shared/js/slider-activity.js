f7c9c279b6cf8e910e5ea05474255b0f52aee54f
kirill2000121212 <alakonnex@gmail.com>
2024-04-25 22:00:56 +0300

    slide ready

---
+++ src/shared/js/slider-activity.js
@@ -0,0 +1,14 @@
+$('.slider__activity').slick({
+    dots: true,
+    infinite: true,
+    speed: 500,
+    fade: true,
+    cssEase: 'linear',
+    autoplay: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    nextArrow: false,
+    prevArrow: false,
+    swipe: true,
+    centerMode: true
+});

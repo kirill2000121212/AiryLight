a259c4bb8ea3eb16c076a2238519f46ad4d1928d
Vladislav <vladislavtihonov790@gmail.com>
2024-04-25 05:27:03 +0300

    add animation and fade dropdown

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import "./shared/scss/animation.scss";
 import "./shared/scss/header.scss";
 import "./shared/scss/sign-up-modal.scss";
 import "./shared/scss/sign-in.scss";
+import "./shared/scss/dropdown.scss";
 import "./shared/scss/footer.scss";
 import "./shared/scss/section-slider.scss";
 
@@ -13,3 +14,4 @@ import "./shared/js/sign-up.js";
 import "./shared/js/sign-in.js";
 import "./shared/js/validate-form.js";
 import "./shared/js/slider-how-work.js";
+import "./shared/js/dropdown-menu.js";
